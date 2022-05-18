ccaf0334f401dcf34f40232692fa468197570525
noormohammad20 <mdsaim204@gmail.com>
2022-05-18 23:55:24 +0600

    first commit

---
+++ src/componets/ToDo.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const ToDo = () => {
+    return (
+        <div>
+            <h2>ToDo</h2>
+        </div>
+    )
+}
+
+export default ToDo
---
+++ src/componets/ToDos.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const ToDos = () => {
+    return (
+        <div>
+            <h2>ToDos</h2>
+        </div>
+    )
+}
+
+export default ToDos
